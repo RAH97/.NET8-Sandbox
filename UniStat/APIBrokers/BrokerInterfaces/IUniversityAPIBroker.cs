df4aa92d32cac882b4ab9cd621f07cf16d635ce6
RAH97 <ryanhering1@gmail.com>
2024-03-08 01:23:11 -0500

    BlazorUniversitySearch Init Commit

---
+++ UniStat/APIBrokers/BrokerInterfaces/IUniversityAPIBroker.cs
@@ -0,0 +1,12 @@
+﻿namespace UniStat.APIBrokers.BrokerInterfaces
+{
+    public interface IUniversityAPIBroker
+    {
+        /// <summary>
+        /// Returns the response message of an asynchronous HTTP Request at the passed in URI.
+        /// </summary>
+        /// <param name="uri"></param>
+        /// <returns></returns>
+        public Task<HttpResponseMessage> Get(Uri uri);
+    }
+}
---
+++ UniStat/APIBrokers/UniversityAPIBroker.cs
@@ -0,0 +1,33 @@
+﻿using UniStat.APIBrokers.BrokerInterfaces;
+using UniversityRankingAPI.Models.Entities;
+
+namespace UniStat.APIBrokers
+{
+    public class UniversityAPIBroker : IUniversityAPIBroker
+    {
+        private readonly ILogger<UniversityAPIBroker> _logger;
+        public UniversityAPIBroker(ILogger<UniversityAPIBroker> logger) 
+        {
+            _logger = logger;
+        }
+        public async Task<HttpResponseMessage> Get(Uri uri)
+        {
+            using(var client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(uri);
+                    response.EnsureSuccessStatusCode();
+                    return response;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex.Message);
+                    throw;
+                }
+            }
+        }
+
+
+    }
+}
